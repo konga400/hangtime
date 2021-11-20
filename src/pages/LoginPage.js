32813dabf9da5ebd9f56b853d974c978b78bd8ef
konga400 <konga400@users.noreply.github.com>
2021-11-20 16:00:50 -0700

    update color of text

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,8 +9,8 @@ function LoginPage() {
                     <Link to='/preferences'>
                         <img className='my-4' src='./images/logo.png' alt="Logo"></img>
                     </Link>
-                    <h1 className='text-white font-bold text-2xl'>Connect & Hangout</h1>
-                    <h1 className='text-white text-xl'>Created by Jordan, Annie, & Spencer</h1>
+                    <h1 className=' text-gray-600 font-bold text-2xl drop-shadow'>Connect & Hangout</h1>
+                    <h1 className='text-gray-600 text-xl drop-shadow'>Created by Jordan, Annie, & Spencer</h1>
                 </div>
                 <div className='absolute -bottom-36 flex flex-col items-center pt-20 rounded-full bg-purple-800 h-3/4 circle-container'>
                     <div className='w-screen flex flex-col items-center justify-center'>
